af7a6b209de06ea490aa4a7000241dbd83f4a508
Jane Costa <yberdysh@gmail.com>
2018-08-09 22:58:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(func){
+	return func()
+}
+
+function setThisWithCall(func, whoDis, args){
+	return func.call(whoDis, args)
+}
+
+function setThisWithApply(cb, whoDis, argsArray){
+	return cb.apply(whoDis, argsArray)
+}
+
+function returnNewFunctionOf(cbCopy, whoDis){
+	return cbCopy.bind(whoDis)
+}
