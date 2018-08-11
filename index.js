f184a5ad28e59c1f8f459d549d1330af91d1be59
github username <github email address>
2018-08-11 12:49:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn(this)
+}
+
+function setThisWithCall(fn, name, age) {
+  return fn.call(name, age)
+}
+
+function setThisWithApply(fn, name, args) {
+  return fn.apply(name,args)
+}
+
+function returnNewFunctionOf(fn, name) {
+  return fn.bind(name)
+}
